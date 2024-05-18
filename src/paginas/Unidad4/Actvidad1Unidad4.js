467ec458d1b1cca3640dfd8881d2891ded930597
denizAM <deniz.a.marin@gmail.com>
2024-05-18 05:25:52 -0600

    Cambios unidad 4

--- src/paginas/Unidad4/Actvidad1Unidad4.js
+++ src/paginas/Unidad4/Actvidad1Unidad4.js
@@ -4,6 +4,7 @@ const Actvidad1Unidad4 = () => {
   return (
     <div>
       <h1>Unidad 4 - Actividad 1</h1>
+      <iframe className='visualizador2' src="https://1drv.ms/b/c/6e5d65377d88b7f4/IQOknIwemZZcR7rtTEePBvItAf0qf3WkI7VRGbhhiI7Mqj8" width="476" height="288" frameborder="0" scrolling="no"></iframe>
     </div>
   )
 }
--- src/paginas/Unidad4/Actvidad2Unidad4.js
+++ src/paginas/Unidad4/Actvidad2Unidad4.js
@@ -4,7 +4,7 @@ const Actvidad2Unidad4 = () => {
   return (
     <div>
         <h1>Unidad 4 - Actividad 2</h1>
-      
+        <iframe className='visualizador2' src="https://1drv.ms/b/c/6e5d65377d88b7f4/IQOAoi8FfvttRI1saNRZtAsTAZk-Mf5yEFGN48bs4lHnl3U" width="476" height="288" frameborder="0" scrolling="no"></iframe>
     </div>
   )
 }
